rzhao271/magiccube4d

commit a36fbbfc9e9d4e0d396439ceedf417fc67055c26

Fix puzzle colour loading issues
- Improves loading of 'facecolors/<Schläfli-symbol>.txt' colour files
- Fixes an issue where colours weren't being reloaded upon puzzle reset

src/com/superliminal/magiccube4d/PuzzleManager.java
@@ -46,6 +46,7 @@ public class PuzzleManager
         if(puzzleDescription == null)
             return;
         puzzleState = VecMath.copyvec(puzzleDescription.getSticker2Face());
+        faceColors = ColorUtils.findColors(puzzleDescription.getSchlafliProduct(), puzzleDescription.nFaces());
     }
 
     //

src/com/superliminal/util/ColorUtils.java
@@ -5,8 +5,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.StringReader;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.StringTokenizer;
@@ -200,31 +198,34 @@ public class ColorUtils {
     private static String colorFilename(String schlafli) {
     	return "facecolors" + File.separator + schlafli + ".txt";
     }
-    
-    private static Color[] findColors(int len, String fname) {
-        for(Color[] cols : readColorLists(fname)) {
+
+    public static Color[] findColors(String schlafli, int len) {
+        String fname = colorFilename(schlafli);
+        URL furl = ResourceUtils.getResource(fname);
+        if (furl == null) {
+            fname = MagicCube.FACE_COLORS_FILE;
+            furl = ResourceUtils.getResource(fname);
+            if (furl == null) {
+                return null;
+            }
+        }
+        return findColors(len, furl);
+    }
+
+    private static Color[] findColors(int len, URL furl) {
+        for(Color[] cols : readColorLists(furl)) {
             if(cols.length == len)
                 return cols;
         }
         return null;
     }
-    
-    public static Color[] findColors(String schlafli, int len) {
-    	String filename = colorFilename(schlafli);
-    	if (!Files.exists(Paths.get(filename)))
-    		filename = MagicCube.FACE_COLORS_FILE;
-    	return findColors(len, filename);
-    }
 
-    private static Color[][] readColorLists(String fname) {
-        URL furl = ResourceUtils.getResource(fname);
-        if(furl == null)
-            return new Color[0][];
+    private static Color[][] readColorLists(URL furl) {
         String contents = ResourceUtils.readFileFromURL(furl);
         //JOptionPane.showMessageDialog(null, contents);
         if(contents == null)
             return new Color[0][];
-        ArrayList<Color[]> colorlines = new ArrayList<Color[]>();
+        ArrayList<Color[]> colorlines = new ArrayList<>();
         try {
             BufferedReader br = new BufferedReader(new StringReader(contents));
             for(String line = br.readLine(); line != null;) {
@@ -246,7 +247,7 @@ public class ColorUtils {
             e.printStackTrace();
         }
         return colorlines.toArray(new Color[0][]);
-    } // end readColorLists()
+    }
 
 
     /**

commit c3f452b576c8b8978e85ae954ff4bcee419b21ac

More packaging and bug fixes (#4)
Packaging fixes:
- Downgrade project files to Java 8 (LTS)
- Add a project jar config and adjust the manifest file

Bug fixes:
- Fix crash caused by a36fbbf
- Change colour scheme file fetching impl to use File instead of URL

.idea/artifacts/magiccube4d_jar.xml
@@ -1,0 +1,8 @@
+<component name="ArtifactManager">
+  <artifact type="jar" name="magiccube4d:jar">
+    <output-path>$PROJECT_DIR$/classes/artifacts/magiccube4d_jar</output-path>
+    <root id="archive" name="magiccube4d.jar">
+      <element id="module-output" name="magiccube4d" />
+    </root>
+  </artifact>
+</component>

.idea/misc.xml
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <project version="4">
-  <component name="ProjectRootManager" version="2" languageLevel="JDK_19" default="true" project-jdk-name="19" project-jdk-type="JavaSDK">
+  <component name="ProjectRootManager" version="2" languageLevel="JDK_1_8" default="true" project-jdk-name="1.8" project-jdk-type="JavaSDK">
     <output url="file://$PROJECT_DIR$/classes" />
   </component>
 </project>

magiccube4d.iml
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <module type="JAVA_MODULE" version="4">
-  <component name="NewModuleRootManager" inherit-compiler-output="true">
+  <component name="NewModuleRootManager" LANGUAGE_LEVEL="JDK_1_8" inherit-compiler-output="true">
     <exclude-output />
     <content url="file://$MODULE_DIR$">
       <sourceFolder url="file://$MODULE_DIR$/src" isTestSource="false" />
     </content>
-    <orderEntry type="inheritedJdk" />
+    <orderEntry type="jdk" jdkName="1.8" jdkType="JavaSDK" />
     <orderEntry type="sourceFolder" forTests="false" />
   </component>
 </module>

src/META-INF/MANIFEST.MF
@@ -1,4 +1,3 @@
 Manifest-Version: 1.0
-Class-Path: .
 Main-Class: com.superliminal.magiccube4d.MC4DLauncher
 

src/com/superliminal/magiccube4d/PuzzleManager.java
@@ -41,12 +41,23 @@ public class PuzzleManager
         resetPuzzleStateNoEvent();
         firePuzzleChanged(false);
     }
+    private void resetPuzzleColors() {
+        Color[] userColors = ColorUtils.findColors(
+                puzzleDescription.getSchlafliProduct(),
+                puzzleDescription.nFaces());
+        if(userColors != null)
+            faceColors = userColors;
+        else if(puzzleDescription.nFaces() == 8)
+            faceColors = MagicCube.DEFAULT_FACE_COLORS;
+        else
+            faceColors = ColorUtils.generateVisuallyDistinctColors(puzzleDescription.nFaces(), .7f, .1f);
+    }
     public void resetPuzzleStateNoEvent() {
         nTwist = iTwist = 0;
         if(puzzleDescription == null)
             return;
         puzzleState = VecMath.copyvec(puzzleDescription.getSticker2Face());
-        faceColors = ColorUtils.findColors(puzzleDescription.getSchlafliProduct(), puzzleDescription.nFaces());
+        resetPuzzleColors();
     }
 
     //
@@ -119,15 +130,7 @@ public class PuzzleManager
                 if(newPuzzle != null) {
                     succeeded = true;
                     puzzleDescription = newPuzzle;
-                    Color[] userColors = ColorUtils.findColors(
-                        puzzleDescription.getSchlafliProduct(),
-                        puzzleDescription.nFaces());
-                    if(userColors != null)
-                        faceColors = userColors;
-                    else if(puzzleDescription.nFaces() == 8)
-                        faceColors = MagicCube.DEFAULT_FACE_COLORS;
-                    else
-                        faceColors = ColorUtils.generateVisuallyDistinctColors(puzzleDescription.nFaces(), .7f, .1f);
+                    resetPuzzleColors();
                     resetPuzzleStateNoEvent();
                 }
                 return null;

src/com/superliminal/util/ColorUtils.java
@@ -1,11 +1,12 @@
 package com.superliminal.util;
 
 import java.awt.Color;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.StringReader;
-import java.net.URL;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.StringTokenizer;
 
@@ -201,52 +202,57 @@ public class ColorUtils {
 
     public static Color[] findColors(String schlafli, int len) {
         String fname = colorFilename(schlafli);
-        URL furl = ResourceUtils.getResource(fname);
-        if (furl == null) {
+        File file = new File(StaticUtils.getBinDir(), fname);
+        if(!file.exists()) {
             fname = MagicCube.FACE_COLORS_FILE;
-            furl = ResourceUtils.getResource(fname);
-            if (furl == null) {
+            file = new File(StaticUtils.getBinDir(), fname);
+            if(!file.exists()) {
                 return null;
             }
         }
-        return findColors(len, furl);
+        return findColors(len, file);
     }
 
-    private static Color[] findColors(int len, URL furl) {
-        for(Color[] cols : readColorLists(furl)) {
+    private static Color[] findColors(int len, File file) {
+        Color[][] lists = readColorLists(file);
+        if(lists == null) {
+            return null;
+        }
+        for(Color[] cols : lists) {
             if(cols.length == len)
                 return cols;
         }
         return null;
     }
 
-    private static Color[][] readColorLists(URL furl) {
-        String contents = ResourceUtils.readFileFromURL(furl);
-        //JOptionPane.showMessageDialog(null, contents);
-        if(contents == null)
-            return new Color[0][];
-        ArrayList<Color[]> colorlines = new ArrayList<>();
+    private static Color[][] readColorLists(File file) {
+        List<String> lines;
         try {
-            BufferedReader br = new BufferedReader(new StringReader(contents));
-            for(String line = br.readLine(); line != null;) {
-                StringTokenizer st = new StringTokenizer(line);
-                Color[] colorlist = new Color[st.countTokens()];
-                for(int i = 0; i < colorlist.length; i++) {
-                    String colstr = st.nextToken();
-                    colorlist[i] = PropertyManager.parseColor(colstr);
-                    if(colorlist[i] == null) {
-                        colorlist = null;
-                        break; // bad line
-                    }
-                }
-                if(colorlist != null)
-                    colorlines.add(colorlist);
-                line = br.readLine();
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
+            lines = Files.readAllLines(file.toPath(), StandardCharsets.US_ASCII);
+        } catch(IOException e) {
+            return null;
         }
-        return colorlines.toArray(new Color[0][]);
+
+        if(lines.isEmpty()) {
+            return null;
+        }
+
+        ArrayList<Color[]> colorlines = new ArrayList<>();
+        for(String line : lines) {
+            StringTokenizer st = new StringTokenizer(line);
+            Color[] colorlist = new Color[st.countTokens()];
+            for(int i = 0; i < colorlist.length; i++) {
+                String colstr = st.nextToken();
+                colorlist[i] = PropertyManager.parseColor(colstr);
+                if(colorlist[i] == null) {
+                    colorlist = null;
+                    break; // bad line
+                }
+            }
+            if(colorlist != null)
+                colorlines.add(colorlist);
+        }
+        return colorlines.toArray(new Color[colorlines.size()][]);
     }
 
 

src/com/superliminal/util/PropertyManager.java
@@ -1,5 +1,6 @@
 package com.superliminal.util;
 
+import java.nio.file.Files;
 import java.util.*;
 import java.io.*;
 import java.awt.*;
@@ -215,7 +216,7 @@ public class PropertyManager extends Properties {
             if(!localPropFile.exists())
                 return; // nothing to load
             try {
-                load(new FileInputStream(localPropFile));
+                load(Files.newInputStream(localPropFile.toPath()));
             } catch(IOException e) {
                 System.err.println("PropertyManager.LocalProps: Could not load local prop file '" + localPropFile.getAbsolutePath() + "'");
                 this.localPropFile = null;
@@ -256,7 +257,7 @@ public class PropertyManager extends Properties {
             if(localPropFile == null || storeFailed)
                 return;
             try {
-                this.store(new FileOutputStream(localPropFile), PRODUCT_NAME + " User Preferences");
+                this.store(Files.newOutputStream(localPropFile.toPath()), PRODUCT_NAME + " User Preferences");
             } catch(IOException e) {
                 storeFailed = true; // so as to only give fail msg once
                 if(!localPropFile.canWrite())