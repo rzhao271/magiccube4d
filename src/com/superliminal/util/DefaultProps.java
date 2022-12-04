23c3e632bc76d0f363aa307b91128989a6eed796
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2022-12-03 21:38:38 -0800

    Change many default properties
    This PR changes many properties to my personal preferences.
    It also changes the default {4,3,3} colour scheme to the one
    used in HactarCE/Hyperspeedcube.

---
+++ src/com/superliminal/util/DefaultProps.java
@@ -0,0 +1,11 @@
+package com.superliminal.util;
+
+public class DefaultProps {
+    public static final boolean AUTO_ROTATE = false;
+    public static final boolean ANTIALIASING = false;
+    public static final boolean DRAW_GROUND = false;
+    public static final boolean DRAW_OUTLINES = false;
+    public static final boolean DRAW_SHADOWS = false;
+    public static final boolean HIGHLIGHT_BY_CUBIE = true;
+    public static final boolean MUTE_SOUND_EFFECTS = true;
+}
